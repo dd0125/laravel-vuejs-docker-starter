42562fe8f5f3bf0079ab80ff1af235a6a4931d10
Daisuke Taji <d_taji@yumemi.co.jp>
2019-10-15 01:24:06 +0900

    Docker一括処理スクリプトを更新

--- docker/rebuild_all_no_cache.sh
+++ docker/rebuild_all_no_cache.sh
@@ -1,5 +1,7 @@
+
+
 docker-compose stop
 docker-compose rm -f
-docker-compose build
+docker-compose build --no-cache
 docker-compose up -d
 docker-compose ps
--- docker/restart_all.sh
+++ docker/restart_all.sh
@@ -1,5 +1,3 @@
 docker-compose stop
-docker-compose rm -f
-docker-compose build
 docker-compose up -d
 docker-compose ps
